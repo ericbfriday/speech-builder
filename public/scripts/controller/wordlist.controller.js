0b55300d37f1f353110ce69ddddcdc9cc7a536e8
Eric Friday <ericfriday@gmail.com>
2017-10-17 13:50:19 -0500

    Basic pop-up occuring on word list webpage. Content not filled in.

--- public/scripts/controller/wordlist.controller.js
+++ public/scripts/controller/wordlist.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('WordListController', function(ListService) {
+myApp.controller('WordListController', function(ListService, WordService, $mdDialog) {
     console.log('in WordListController Controller');
     
     const vm = this;
@@ -8,4 +8,37 @@ myApp.controller('WordListController', function(ListService) {
         console.log('Inside getWordList function/WordList controller');
         ListService.getWords();
     };
+
+    vm.detailPopup = function (ev, word) {
+        // console.log('logging word in detailPopup ', word);
+        
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: '/views/wordDetails.tmpl.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: vm.customFullscreen // Only for -xs, -sm breakpoints.
+          });
+        //   .then(function(answer) {
+        //     $scope.status = 'You said the information was "' + answer + '".';
+        //   }, function() {
+        //     $scope.status = 'You cancelled the dialog.';
+        //   });
+    };
+
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function() {
+          $mdDialog.hide();
+        };
+    
+        $scope.cancel = function() {
+          $mdDialog.cancel();
+        };
+    
+        $scope.answer = function(answer) {
+          $mdDialog.hide(answer);
+        };
+      }
+    
  });
