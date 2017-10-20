4da14020b19a2a4b1021a4ebd0b5daaad0f24ec5
Eric Friday <ericfriday@gmail.com>
2017-10-20 12:47:16 -0500

    added radio buttons and default values

--- public/scripts/controller/reporting.controller.js
+++ public/scripts/controller/reporting.controller.js
@@ -1,12 +1,22 @@
-myApp.controller('ReportingController', function($http, $scope) {
+myApp.controller('ReportingController', function ($http, $scope) {
     console.log('in Reporting Controller');
-     
+
     const vm = this;
 
     vm.report = {
-        name: '',
+        studentName: '',
         word: '',
-        summary: ''
+        summary: '',
+        attempt1: 'na',
+        attempt2: 'na',
+        attempt3: 'na',
+        attempt4: 'na',
+        attempt5: 'na',
+        attempt6: 'na',
+        attempt7: 'na',
+        attempt8: 'na',
+        attempt9: 'na',
+        attempt10: 'na'
     };
 
- });
+});
