3a3b440295b9736fc7567d03c051f4bfa030fd0e
Eric Friday <ericfriday@gmail.com>
2017-10-23 14:47:26 -0500

    Successfully querying db to update outcome of each opportunity at a sound.

--- public/scripts/services/reporting.service.js
+++ public/scripts/services/reporting.service.js
@@ -3,62 +3,104 @@ myApp.service('ReportingService', function ($http, $firebaseAuth) {
     const auth = $firebaseAuth();
     const database = firebase.database();
 
-    // sv.report = {};
+    sv.report = {};
     sv.currentStudent = {
         data: ''
     };
     sv.studiedWords = [];
     sv.wordObj = {
         instructor: '',
+        instructorName: '',
         studentName: sv.currentStudent.data,
+        date: '',
         word: '',
-        totalAttempts: 0,
-        satisfactory: 0,
-        prompt: 0,
-        unsatisfactory: 0
+        result: ''//,
+        // totalAttempts: 0,
+        // satisfactory: 0,
+        // prompt: 0,
+        // unsatisfactory: 0
     };
+
+    var dateObj = new Date();
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+    var newdate = year + "/" + month + "/" + day;
 
     sv.opportunityReport = function (studyWord, outcome) {
         // console.log('logging word, student, and outcome -> ', studyWord, sv.currentStudent.data, outcome);
         sv.wordObj.instructor = firebase.auth().currentUser.uid;
-        sv.wordObj.studentName = sv.currentStudent.data;
+        sv.wordObj.instructorName = firebase.auth().currentUser.displayName;
+        sv.wordObj.student = sv.currentStudent.data;
         sv.wordObj.word = studyWord;
-        sv.wordObj.totalAttempts++;
-        if (sv.studyWord == sv.studiedWords.data) {
-            // console.log('loggin studiedWords[i] -> ', sv.studiedWords[i].word);
+        sv.wordObj.date = newdate;
+        sv.wordObj.outcome = outcome;
+        console.log('logging wordObj -> ', sv.wordObj);
 
-            if (outcome === 'satisfactory') {
-                sv.wordObj.satisfactory++;
-            } else if (outcome === 'prompted') {
-                sv.wordObj.prompt++;
-            } else if (outcome === 'unsatisfactory') {
-                sv.wordObj.unsatisfactory++;
+        $http.post('/reporting', sv.wordObj)
+        .then((response) => {
+            console.log('response -->', response);
+            if (response.status === 203) {
+                // ITEM CREATED!
+                console.log('CREATED!');
             } else {
-                console.log('Not a match in studyword index -> ', i);
+                console.log('Error in POST route reporting.service.js');
+                
             }
-        }
-        for (let i = 0; i < sv.studiedWords.length; i++) {
-            console.log('running for loop');
-            
-        }
+        }); // end POST
+    };
+        // on load -> pull list of today's reports from server. Keep static in router.
+        
+        // Query for teacher matching results.
+        // Query for student matching results.
+        // Query for word matching results. 
+        // update totals and tries of words.
 
-        // push wordObj into array if it's a new word, otherwise update existing word within array.
 
-        if (sv.studiedWords.length === 0){
-            sv.studiedWords.push(sv.wordObj);
-        } else {
-            for (i=0;i<sv.studiedWords.length;i++) {
-                if (sv.studiedWords[i].word === studyWord){
-                    sv.studiedWords.splice(index,1, sv.wordObj);
-                    // sv.studiedWords.push(sv.wordObj);
-                }
-            }
-        }
+        // if teacher matches -> continue update
+        // else teacher doesn't match -> new object with traits
+
+        // if student matches -> continue update
+        // else studen't doesn't match -> new object with traits
+
+        // if word matches -> continue update
+        // else if word doesn't match -> new object with traits
+
+
+        // if (sv.studyWord == sv.studiedWords.data) {
+        //     // console.log('loggin studiedWords[i] -> ', sv.studiedWords[i].word);
+
+        //     if (outcome === 'satisfactory') {
+        //         sv.wordObj.satisfactory++;
+        //     } else if (outcome === 'prompted') {
+        //         sv.wordObj.prompt++;
+        //     } else if (outcome === 'unsatisfactory') {
+        //         sv.wordObj.unsatisfactory++;
+        //     } else {
+        //         console.log('Not a match in studyword index -> ', i);
+        //     }
+        // }
+        // for (let i = 0; i < sv.studiedWords.length; i++) {
+        //     console.log('running for loop');
+        // }
+
+        // // push wordObj into array if it's a new word, otherwise update existing word within array.
+
+        // if (sv.studiedWords.length === 0){
+        //     sv.studiedWords.push(sv.wordObj);
+        // } else {
+        //     for (i=0;i<sv.studiedWords.length;i++) {
+        //         if (sv.studiedWords[i].word === studyWord){
+        //             sv.studiedWords.splice(index,1, sv.wordObj);
+        //             // sv.studiedWords.push(sv.wordObj);
+        //         }
+        //     }
+        // }
 
 
         
-    console.log('Studied Words in reporting service -> ', sv.studiedWords);
-    };
+    // console.log('Studied Words in reporting service -> ', sv.studiedWords);
+    // };
 
     // // outcome logic
     // if (outcome === 'satisfactory') {
--- server/routes/reporting.js
+++ server/routes/reporting.js
@@ -8,34 +8,65 @@ const pool = require('../modules/pool');
 // const db = firebase.database();
 
 router.post('/', function (req, res) {
-    // Data arrives as follows from word.service.js -> data: {word: studyWord, student: sv.studentName.data, outcome: outcome}
-    // console.log('in the reporting get', req);
     word = req.body.word;
     student = req.body.student;
     outcome = req.body.outcome;
     instructor = req.body.instructor;
-    user_id = req.decodedToken.user_id;
-    date = new Date();
-    console.log('Logging reporting POST route variables word, student, outcome, instructor, date -> ', word, student, outcome, instructor, date, user_id);
+    instructorName = req.body.instructorName;
+    date = req.body.date;
+    queries = [instructor, instructorName, student, word, date]; // does not currently include outcome.
+    queryString = "UPDATE word_reports SET totalattempts = totalattempts + 1, " + outcome + " = " + outcome + " + 1 WHERE instructor = '" + instructor + "' AND student = '" + student + "' AND word = '" + word + "';";
+    // console.log('Logging reporting POST route variables word, student, outcome, instructor, date -> ', word, student, outcome, instructor, instructorName, date);
     
-    // commented out until Schema is established
-    // pool.connect(function (conErr, client, done){
-    //     if (conErr){
-    //         console.log(conErr);
-    //         res.sendStatus(500);
-    //     } else { // NEED TO REPLACE BELOW WITH INSERT FUNCTION
-    //         client.query('SELECT * FROM reporting_list ORDER BY id; ', function (queryErr, resultObj){
-    //             done();
-    //             if (queryErr){
-    //                 console.log(queryErr);
-    //                 res.sendStatus(500);
-    //             } else {
-    //                 // console.log(resultObj.rows);
-    //                 res.send(resultObj.rows);
-    //             }
-    //         });
-    //     }
-    // });
+    pool.connect(function (conErr, client, done){
+        if (conErr){
+            console.log(conErr);
+            res.sendStatus(500);
+        } else { // NEED TO REPLACE BELOW WITH INSERT FUNCTION
+            client.query("SELECT * FROM word_reports ORDER BY id; ", function (queryErr, resultObj){
+                done();
+                if (queryErr){
+                    console.log(queryErr);
+                    res.sendStatus(500);
+                } else {
+                    reports = resultObj.rows;
+                    // console.log(resultObj.rows);
+                    // loop through resultObj and search for results with matching instructor. Then matching student, then matching word. 
+                    for (i=0; i<reports.length;i++){ // for loop matching instructors
+                        if(reports[i].instructor == instructor){ // checks for matching instructors
+                            for (i=0; i<reports.length;i++){ // for loop matching student
+                                if(reports[i].student == student){ // checks for matching students
+                                    for (i=0; i<reports.length;i++){ // for loop matching words
+                                        if(reports[i].word == word){ // checks for matching words
+                                            // console.log('Logging Query String -> ', queryString);
+                                            client.query(queryString, function (queryErr, resultObj){ // Queries DB to update appropriate outcomes
+                                            done();
+                                            if (queryErr){
+                                                console.log(queryErr);
+                                                res.sendStatus(500);
+                                            } else {
+                                                res.sendStatus(203);
+                                            }});
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    // for loop matching instructors, if found, proceed to update.
+                    // for loop matching student, if found, proceed to update.
+                    // for loop matching word, if found,  proceed to update.
+                    // matching everything. Proceed to update. Update totalAttempts & corresponding column by totalAttempts=totalAttempts+1, etc...
+                    // else for loop matching word, if word not found, INSERT function from req.
+                    // else for loop matching student, if student not found, INSERT function from req.
+                    // else for loop matching instructor, if instructor not found, INSERT function from req.
+                    // outside of all loops -> query DB for matching instructor, student, word and return results via res.send(resultObj.rows);
+
+                    // res.send(resultObj.rows);
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
